92b5b050148beebb362e19cf2c4778c0c8496c57
Tristan Zajonc <tristanz@gmail.com>
2013-12-29 17:58:59 -0500

    Tweak tests.

--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ test('should return false for invalid email', function(){
   assert.equal(false, validate.isEmail()('test@email'));
 });
 
-test('should return true valid chain', function(){
+test('should return true for valid chain', function(){
   assert.equal(true, validate.len(6, 64).isEmail()('test@email.com'));
 });
 
-test('should return false invalid chain', function(){
+test('should return false for invalid chain', function(){
   assert.equal(false, validate.len(60, 100).isEmail()('test@email.com'));
 });
